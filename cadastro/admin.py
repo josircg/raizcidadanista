8d3ab371d61fe85ce28257474c0f190470477076
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-05-18 14:59:40 -0300

    Proteção do inline dos Círculo

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,29 +1,21 @@
 # -*- coding:utf-8 -*-
 from django.contrib import admin
+from django.contrib.admin.util import flatten_fieldsets
 
-from models import *
+from models import Membro, Circulo, CirculoMembro, CirculoEvento, UF, Pessoa
 
-class CirculoMembroInline(admin.TabularInline):
+
+class CirculoMembroMembroInline(admin.TabularInline):
     model = CirculoMembro
     extra = 1
     verbose_name = u'Círculo do Membro'
     verbose_name_plural = u'Círculos do Membro'
 
-    def get_readonly_fields(self, request, obj=None):
-        if request.user.is_superuser:
-            return ()
-
-        membro = Membro.objects.get(usuario = request.user)
-        if request.user.groups.filter(name=u'Comissão').exists() or CirculoMembro.objects.filter(membro=membro,administrador=True).exists():
-            return ()
-        else:
-            return ('administrador',)
-
 class MembroAdmin(admin.ModelAdmin):
     list_filter = ('uf',)
     search_fields = ('nome','email',)
     list_display = ('nome', 'email', 'dtcadastro', 'aprovador',)
-    inlines = (CirculoMembroInline,)
+    inlines = (CirculoMembroMembroInline, )
     actions = ('aprovacao',)
 
     def aprovacao(self, request, queryset):
@@ -41,66 +33,67 @@ class MembroAdmin(admin.ModelAdmin):
             return ()
         else:
             return ('usuario', 'facebook_id','aprovador',)
+admin.site.register(Membro, MembroAdmin)
 
-class CirculoEventoInline(admin.TabularInline):
-# só permitir alterar o administrador se o usuário também for administrador
+
+class CirculoMembroCirculoInline(admin.TabularInline):
+    model = CirculoMembro
+    extra = 1
+    verbose_name = u'Membro do Círculo'
+    verbose_name_plural = u'Membros do Círculo'
+
+class CirculoEventoCirculoInline(admin.TabularInline):
     model = CirculoEvento
     extra = 1
     verbose_name = u'Evento do Círculo'
     verbose_name_plural = u'Círculos do Membro'
 
     def get_readonly_fields(self, request, obj=None):
-        if Circulo.objects.filter(circulo=self.circulo, membro__usuario=request.user,administrador=True).count() == 0 and request.user.groups.filter(name=u'Comissão').count() == 0:
-            return ('nome','dt_evento','local')
+        if not (CirculoMembro.objects.filter(circulo=obj, membro__usuario=request.user, administrador=True).exists() or request.user.groups.filter(name=u'Comissão').exists()):
+            return ('nome', 'dt_evento', 'local')
         else:
             return ()
 
 class CirculoAdmin(admin.ModelAdmin):
     list_filter = ('uf',)
 
-#    fieldset_owner = (None, { "fields" : (('titulo','descricao','tipo','uf','municipio','oficial','dtcadastro','site_externo'),) }, )
-#    fieldsets = (None, { "fields" : (('titulo','descricao','uf','municipio','site_externo',),)})
+    fieldsets_owner = (
+        (None, {"fields" : ('titulo', 'descricao', 'tipo', 'uf', 'municipio', 'oficial', 'dtcadastro', 'site_externo',),},),
+    )
+    fieldsets = (
+        (None, {"fields" : ('titulo', 'descricao', 'uf', 'municipio', 'site_externo',),}, ),
+    )
 
-#    def get_fieldsets(self, request, obj=None):
-#        if request.user.is_superuser:
-#            return self.fieldsets_owner
-#        return self.fieldsets
+    def get_fieldsets(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.fieldsets_owner
+        return self.fieldsets
 
     def save_model(self, request, obj, form, change):
         obj.save()
         if not change:
             membro = Membro.objects.get(usuario=request.user)
-            admin = CirculoMembro()
-            admin.membro = membro
-            admin.circulo = obj
-            admin.administrador = True
-            admin.save()
-
-'''    def get_readonly_fields(self, request, obj=None):
-        if CirculoMembro.objects.filter(membro__usuario=request.user,administrador=True).count() == 0 and request.user.groups.filter(name=u'Comissão').count() == 0:
-
-            if self.declared_fieldsets:
-                return flatten_fieldsets(self.declared_fieldsets)
-            else:
-                return list(set(
-                    [field.name for field in self.opts.local_fields] +
-                    [field.name for field in self.opts.local_many_to_many]
-                ))
-
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        if CirculoMembro.objects.filter(membro__usuario=request.user).count() == 0 and request.user.groups.filter(name=u'Comissão').count() == 0:
-            self.inlines = [CirculoEventoInline,]
+            CirculoMembro(
+                membro = membro,
+                circulo = obj,
+                administrador = True,
+            ).save()
+    
+    def get_inline_instances(self, request, obj=None):
+        if request.user.groups.filter(name=u'Comissão').exists() or CirculoMembro.objects.filter(circulo=obj, membro__usuario=request.user, administrador=True).exists():
+            self.inlines = [CirculoEventoCirculoInline, CirculoMembroCirculoInline, ]
         else:
-            self.inlines = [CirculoEventoInline,CirculoMembroInline, ]
+            self.inlines = [CirculoEventoCirculoInline,]
+        return [inline(self.model, self.admin_site) for inline in self.inlines]
 
-        self.inline_instances = []
-        for inline_class in self.inlines:
-            inline_instance = inline_class(self.model, self.admin_site)
-            self.inline_instances.append(inline_instance)
-        return super(CirculoAdmin, self).change_view(request, object_id, form_url)
-'''
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name=u'Comissão').exists() or CirculoMembro.objects.filter(circulo=obj, membro__usuario=request.user, administrador=True).exists():
+            return ()
+        else:
+            return flatten_fieldsets(self.get_fieldsets(request, obj))
+
+admin.site.register(Circulo, CirculoAdmin)
+
 
 admin.site.register(UF)
 admin.site.register(Pessoa)
-admin.site.register(Circulo, CirculoAdmin)
-admin.site.register(Membro, MembroAdmin)

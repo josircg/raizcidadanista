b5da35a6dc1c7d70e9cbecfa630a38175b9d9e4a
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-06-17 10:06:21 -0300

    Ajustes no site

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from cadastro import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='site/index.html'), name="index"),
+    url(r'^$', views.IndexView.as_view(), name="index"),
     url(r'^newsletter/$', views.NewsletterView.as_view(), name="newsletter"),
     url(r'^membro/$', views.MembroView.as_view(), name="membro"),
 ]

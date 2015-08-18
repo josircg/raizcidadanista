2209363214e2653fbe439a947ba5fe2efc450f89
josircg <josircg@gmail.com>
2015-08-17 21:09:52 -0300

    Formulários

--- raizcidadanista/cadastro/forms.py
+++ raizcidadanista/cadastro/forms.py
@@ -7,9 +7,16 @@ from models import Pessoa, Membro
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Pessoa
-        fields = ('nome', 'uf', 'municipio', 'email', 'sexo', 'celular', 'residencial', )
+        fields = ('nome', 'email', 'uf', 'municipio', 'sexo', )
 
 class MembroForm(forms.ModelForm):
+    class Meta:
+        model = Membro
+        fields = ('nome', 'uf', 'municipio', 'email', 'sexo', 'celular', 'residencial',
+            'atividade_profissional', 'dtnascimento', 'rg', 'titulo_eleitoral',
+            'uf_eleitoral', 'municipio_eleitoral', 'filiacao_partidaria', )
+
+class FiliadoForm(forms.ModelForm):
     class Meta:
         model = Membro
         fields = ('nome', 'uf', 'municipio', 'email', 'sexo', 'celular', 'residencial',

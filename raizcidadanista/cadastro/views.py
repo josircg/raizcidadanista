badeb202553f11b4f8bad24c7adf8b3ed0f39a29
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-09-07 08:54:36 -0300

    Colocar Item do Menu colapsado no smartphone #14

--- raizcidadanista/cadastro/views.py
+++ raizcidadanista/cadastro/views.py
@@ -2,10 +2,14 @@
 from django.views.generic import FormView, TemplateView
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.http import HttpResponse
+from django.utils.http import int_to_base36, base36_to_int
+from django.utils.crypto import constant_time_compare, salted_hmac
+from django.utils import simplejson
 
-from models import Circulo
+from models import Circulo, Membro
 from municipios.models import UF
-from forms import NewsletterForm, MembroForm, FiliadoForm
+from forms import NewsletterForm, MembroForm, FiliadoForm, FiliadoAtualizarLinkForm, FiliadoAtualizarForm
 
 
 class NewsletterView(FormView):
@@ -19,6 +23,10 @@ class NewsletterView(FormView):
         return self.response_class(
             request=self.request,
             template=self.template_success_name,
+            context={
+                'title': u'Bem vindo',
+                'msg': u'Obrigado por se cadastrar!',
+            }
         )
 
     def form_invalid(self, form):
@@ -37,6 +45,10 @@ class MembroView(FormView):
         return self.response_class(
             request=self.request,
             template=self.template_success_name,
+            context={
+                'title': u'Bem vindo',
+                'msg': u'Obrigado por se cadastrar!',
+            }
         )
 
     def form_invalid(self, form):
@@ -44,10 +56,115 @@ class MembroView(FormView):
         return super(MembroView, self).form_invalid(form)
 
 
+class FiliadoAtualizarLinkView(FormView):
+    template_name = 'cadastro/filiado-atualizar-link.html'
+    template_success_name = 'cadastro/bem-vindo.html'
+    template_email_name = 'cadastro/emails/filiado-atualizar.html'
+    form_class = FiliadoAtualizarLinkForm
+
+    def get_form_kwargs(self):
+        kwargs = super(FiliadoAtualizarLinkView, self).get_form_kwargs()
+        kwargs['initial'] = self.request.GET
+        return kwargs
+
+    def form_valid(self, form):
+        form.sendmail(template_email_name=self.template_email_name)
+        return self.response_class(
+            request=self.request,
+            template=self.template_success_name,
+            context={
+                'title': u'Atualização de Cadastro do Raíz',
+                'msg': u'Um email acaba de ser enviado com as instruções para edição dos seus dados',
+            }
+        )
+
+    def form_invalid(self, form):
+        messages.error(self.request, u"Preencha corretamente todos os dados!")
+        return super(FiliadoAtualizarLinkView, self).form_invalid(form)
+
+
+class FiliadoAtualizarView(FormView):
+    template_name = 'cadastro/filiado-atualizar.html'
+    template_success_name = 'cadastro/bem-vindo.html'
+    form_class = FiliadoAtualizarForm
+
+    def get_instance(self, request, uidb36, token):
+        def create_token(instance):
+            key_salt = "cadastro.forms.FiliadoAtualizarLinkForm"
+            value = u'%s%s' % (instance.pk, instance.email,)
+            return salted_hmac(key_salt, value).hexdigest()[::2]
+
+        try:
+            uid_int = base36_to_int(uidb36)
+            instance = Membro.objects.get(id=uid_int)
+        except (ValueError, Membro.DoesNotExist):
+            return None
+        if not constant_time_compare(create_token(instance), token):
+            return None
+
+        return instance
+
+    def get(self, request, uidb36, token, *args, **kwargs):
+        self.instance = self.get_instance(request, uidb36, token)
+        if not self.instance:
+            return self.response_class(
+                request=self.request,
+                template=self.template_success_name,
+                context={
+                    'title': u'Link inválido, tente novamente.',
+                    'msg': u'Link inválido, tente novamente.',
+                }
+            )
+        return super(FiliadoAtualizarView, self).get(request, *args, **kwargs)
+
+    def post(self, request, uidb36, token, *args, **kwargs):
+        self.instance = self.get_instance(request, uidb36, token)
+        if not self.instance:
+            return self.response_class(
+                request=self.request,
+                template=self.template_success_name,
+                context={
+                    'title': u'Link inválido, tente novamente.',
+                    'msg': u'Link inválido, tente novamente.',
+                }
+            )
+        return super(FiliadoAtualizarView, self).post(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(FiliadoAtualizarView, self).get_form_kwargs()
+        kwargs['instance'] = self.instance
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        messages.info(self.request, u"Cadastro atualizado com sucesso!")
+        return self.response_class(
+            request=self.request,
+            template=self.template_success_name,
+            context={
+                'title': u'Cadastro atualizado com sucesso!',
+                'msg': u'Obrigado por atualizar o seu cadastro.',
+            }
+        )
+
+    def form_invalid(self, form):
+        messages.error(self.request, u"Preencha corretamente todos os dados!")
+        return super(FiliadoAtualizarView, self).form_invalid(form)
+
+
 class FiliadoView(FormView):
     template_name = 'cadastro/filiado.html'
     template_success_name = 'cadastro/bem-vindo.html'
     form_class = FiliadoForm
+
+    def get(self, request, *args, **kwargs):
+        if request.GET.get('email'):
+            json = {'msg': ''}
+            if Membro.objects.filter(email=request.GET.get('email')).exists():
+                json['msg'] = u'Você já está cadastrado. Deseja editar os seus dados atuais? <a href="%s?email=%s">Clique aqui</a>' % (reverse('filiado_atualizar_link'), request.GET.get('email'))
+            return HttpResponse(simplejson.dumps(json, ensure_ascii=False), mimetype='text/javascript; charset=utf-8')
+
+        return super(FiliadoView, self).get(request, *args, **kwargs)
 
     def form_valid(self, form):
         form.save()
@@ -55,6 +172,10 @@ class FiliadoView(FormView):
         return self.response_class(
             request=self.request,
             template=self.template_success_name,
+            context={
+                'title': u'Bem vindo',
+                'msg': u'Obrigado por se cadastrar!',
+            }
         )
 
     def form_invalid(self, form):

da79802d9fe72c0f9b2f5336901f7797a278f347
josircg <josircg@gmail.com>
2015-09-10 02:34:59 -0300

    Circulos Identitários

--- raizcidadanista/cadastro/admin.py
+++ raizcidadanista/cadastro/admin.py
@@ -10,6 +10,11 @@ from models import Membro, Circulo, CirculoMembro, CirculoEvento, Pessoa
 
 from poweradmin.admin import PowerModelAdmin, PowerButton
 
+class PessoaAdmin(PowerModelAdmin):
+    list_filter = ('uf', 'dtcadastro')
+    search_fields = ('nome', 'email',)
+    list_display = ('nome', 'email', 'dtcadastro', )
+
 class CirculoMembroMembroInline(admin.TabularInline):
     model = CirculoMembro
     extra = 1
@@ -57,6 +62,7 @@ class CirculoEventoCirculoInline(admin.TabularInline):
             return ()
 
 class CirculoAdmin(PowerModelAdmin):
+    search_fields = ('titulo',)
     list_display = ('titulo', 'tipo', 'uf', 'oficial',)
     list_filter = ('tipo','uf',)
     fieldsets_comissao = (
@@ -130,4 +136,4 @@ class CirculoAdmin(PowerModelAdmin):
 admin.site.register(Circulo, CirculoAdmin)
 admin.site.register(CirculoEvento)
 admin.site.register(Membro, MembroAdmin)
-admin.site.register(Pessoa)
+admin.site.register(Pessoa, PessoaAdmin)

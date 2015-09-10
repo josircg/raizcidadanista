dd818fc5c7814b9188ab8a694ad423fead94a284
josircg <josircg@gmail.com>
2015-09-10 02:37:34 -0300

    Circulos Identitários

--- raizcidadanista/settings/test.py
+++ raizcidadanista/settings/test.py
@@ -19,13 +19,3 @@ ALLOWED_HOSTS = ['site4.irdx.com.br', 'teste.raiz.org.br', ]
 
 SITE_HOST = 'http://teste.raiz.org.br'
 
-#Configurations for send email
-REPLY_TO_EMAIL = 'raizmovimentocidanista@gmail.com'
-DEFAULT_FROM_EMAIL = 'Raiz Cidadanista<raizmovimentocidanista@gmail.com>'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'raiz'
-EMAIL_HOST_PASSWORD = 'raiz2016'
-EMAIL_SUBJECT_PREFIX = u'Raiz Cidadanista - '
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
-

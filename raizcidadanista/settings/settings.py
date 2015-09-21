f4d18dae847beb136cf3c48a0b19eb3a9691034d
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-09-21 11:07:42 -0300

    Implantar variável USERNAME no Log #34

--- raizcidadanista/settings/settings.py
+++ raizcidadanista/settings/settings.py
@@ -102,6 +102,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'utils.middleware.ExceptionUserInfoMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

124ff6a7b6e66b34a5ebf1e226f3269435800cd4
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-09-30 08:50:52 -0300

    Incluir módulos da Campanha e Listas na app de cadastro #37

--- raizcidadanista/cadastro/models.py
+++ raizcidadanista/cadastro/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User, Group
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.conf import settings
-from django.db.models import signals
+from django.db.models import signals, F
 from django.dispatch import receiver
 
 from django.core.mail import EmailMultiAlternatives

670418ad8212f0ddb987fb2d9819e5e4e7a9f67d
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-10-19 11:58:25 -0300

    Formulários de Visitantes / Colaborador / Filiado #21

--- raizcidadanista/cadastro/urls.py
+++ raizcidadanista/cadastro/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^membro/entrar-circulo/(?P<circulo_id>\d+)/$', login_required(views.MembroEntrarCirculoView.as_view()), name="membro_entrar_circulo"),
     url(r'^filiado/$', views.FiliadoView.as_view(), name="filiado"),
     url(r'^filiado/atualizar/$', views.FiliadoAtualizarLinkView.as_view(), name="filiado_atualizar_link"),
-    url(r'^filiado/atualizar/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,20})/$', views.FiliadoAtualizarView.as_view(), name="filiado_atualizar"),
+    url(r'^filiado/atualizar/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<ts_b36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,20})/$', views.FiliadoAtualizarView.as_view(), name="filiado_atualizar"),
     url(r'^validar-email/(?P<pessoa_id>\d+)/$', views.ValidarEmailView.as_view(), name="validar_email"),
     url(r'^campanha/(?P<pk>\d+)/$', views.CampanhaView.as_view(), name='campanha_views'),
 ]

1c926ad0975cfaad54a7a03a047eb10e7405463a
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-10-28 08:59:10 -0300

    Removendo formulários

--- raizcidadanista/cadastro/urls.py
+++ raizcidadanista/cadastro/urls.py
@@ -6,12 +6,12 @@ from django.contrib.auth.decorators import login_required
 from cadastro import views
 
 urlpatterns = [
-    url(r'^newsletter/$', views.NewsletterView.as_view(), name="newsletter"),
-    url(r'^membro/$', views.MembroView.as_view(), name="membro"),
-    url(r'^membro/entrar-circulo/(?P<circulo_id>\d+)/$', login_required(views.MembroEntrarCirculoView.as_view()), name="membro_entrar_circulo"),
-    url(r'^filiado/$', views.FiliadoView.as_view(), name="filiado"),
-    url(r'^filiado/atualizar/$', views.FiliadoAtualizarLinkView.as_view(), name="filiado_atualizar_link"),
-    url(r'^filiado/atualizar/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<ts_b36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,20})/$', views.FiliadoAtualizarView.as_view(), name="filiado_atualizar"),
-    url(r'^validar-email/(?P<pessoa_id>\d+)/$', views.ValidarEmailView.as_view(), name="validar_email"),
-    url(r'^campanha/(?P<pk>\d+)/$', views.CampanhaView.as_view(), name='campanha_views'),
+    # url(r'^newsletter/$', views.NewsletterView.as_view(), name="newsletter"),
+    # url(r'^membro/$', views.MembroView.as_view(), name="membro"),
+    # url(r'^membro/entrar-circulo/(?P<circulo_id>\d+)/$', login_required(views.MembroEntrarCirculoView.as_view()), name="membro_entrar_circulo"),
+    # url(r'^filiado/$', views.FiliadoView.as_view(), name="filiado"),
+    # url(r'^filiado/atualizar/$', views.FiliadoAtualizarLinkView.as_view(), name="filiado_atualizar_link"),
+    # url(r'^filiado/atualizar/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<ts_b36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,20})/$', views.FiliadoAtualizarView.as_view(), name="filiado_atualizar"),
+    # url(r'^validar-email/(?P<pessoa_id>\d+)/$', views.ValidarEmailView.as_view(), name="validar_email"),
+    # url(r'^campanha/(?P<pk>\d+)/$', views.CampanhaView.as_view(), name='campanha_views'),
 ]

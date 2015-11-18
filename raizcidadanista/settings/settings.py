a1ee454059cd51eda36df1707567b467583ae01b
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-11-18 08:35:18 -0300

    Fluxo de Aprovação de Artigos #31

--- raizcidadanista/settings/settings.py
+++ raizcidadanista/settings/settings.py
@@ -107,6 +107,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'utils.middleware.Http403Middleware',
     'utils.ThreadLocal.ThreadLocalMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     'django.middleware.clickjacking.XFrameOptionsMiddleware',

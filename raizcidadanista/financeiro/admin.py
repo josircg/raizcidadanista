76e61e83085cf6b3cb9215f08757786f0a3b7509
Leonardo C. Santos <leonardocsantoss@gmail.com>
2015-12-21 08:15:31 -0300

    Action Listagem de Doadores na tela de Receitas #54

--- raizcidadanista/financeiro/admin.py
+++ raizcidadanista/financeiro/admin.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.template.loader import get_template
+from django.template.context import RequestContext
+from django.http import HttpResponse
+from django.db.models import Sum
+
+import cStringIO as StringIO
+import cgi
+from xhtml2pdf.pisa import pisaDocument
 
 from poweradmin.admin import PowerModelAdmin, PowerButton
 
@@ -31,9 +39,24 @@ class ReceitaAdmin(PowerModelAdmin):
         ('q1', u'Conta', ['conta__conta', ]),
         ('q2', u'Colaborador', ['colaborador__nome', ]),
     )
-
     fieldsets = [
         (None, {'fields': ('conta', 'colaborador', )}),
         (u'Datas e Valor', {'fields': ('dtaviso', 'valor', 'dtpgto', )}),
     ]
+    actions = ('listagem_doadores', )
+
+    def listagem_doadores(self, request, queryset, template_name_pdf='admin/financeiro/receita/listagem-doadores-pdf.html'):
+        template = get_template(template_name_pdf)
+        context = RequestContext(request, {
+            'results': queryset,
+            'total': queryset.aggregate(total=Sum('valor')).get('total', 0)
+        })
+        html  = template.render(context)
+
+        dataresult = StringIO.StringIO()
+        pdf = pisaDocument(StringIO.StringIO(html.encode("UTF-8")), dest=dataresult)
+        if not pdf.err:
+            return HttpResponse(dataresult.getvalue(), mimetype='application/pdf')
+        return HttpResponse('We had some errors<pre>%s</pre>' % cgi.escape(html))
+    listagem_doadores.short_description = u'Listagem Doadores'
 admin.site.register(Receita, ReceitaAdmin)

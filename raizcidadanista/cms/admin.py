982dc74a6424307053243b79dfff4fffedaf2382
Leonardo C. Santos <leonardocsantoss@gmail.com>
2016-02-01 11:52:49 -0300

    Fluxo de Aprovação de Artigos #31

--- raizcidadanista/cms/admin.py
+++ raizcidadanista/cms/admin.py
@@ -195,7 +195,7 @@ class ArticleAdmin(PowerModelAdmin):
 
     def queryset(self, request):
         qs = super(ArticleAdmin, self).queryset(request)
-        if (request.user.has_perm('cms.manage_articles') or request.user.groups.filter(name=u'Editor').exists()) and not request.user.is_superuser:
+        if request.user.has_perm('cms.manage_articles') and not request.user.is_superuser:
             qs = qs.filter(author=request.user)
         return qs
 

fb9cdc8700f90f71154475ecb2ca5be73cc737de
Leonardo C. Santos <leonardocsantoss@gmail.com>
2016-05-10 14:50:09 -0300

    Action "Atualização Cadastral" na tela dos Membros #49

--- raizcidadanista/cadastro/urls.py
+++ raizcidadanista/cadastro/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^membro/entrar-circulo/(?P<circulo_id>\d+)/$', login_required(views.MembroEntrarCirculoView.as_view()), name="membro_entrar_circulo"),
     url(r'^filiado/$', views.FiliadoView.as_view(), name="filiado"),
     url(r'^atualizar-cadastro/$', views.AtualizarCadastroLinkView.as_view(), name="atualizar_cadastro_link"),
-    url(r'^atualizar-cadastro/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<ts_b36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,20})/$', views.AtualizarCadastroView.as_view(), name="atualizar_cadastro"),
+    url(r'^atualizar-cadastro/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<ts_b36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,20})/?$', views.AtualizarCadastroView.as_view(), name="atualizar_cadastro"),
     url(r'^validar-email/(?P<pessoa_id>\d+)/$', views.ValidarEmailView.as_view(), name="validar_email"),
     url(r'^campanha/(?P<pk>\d+)/$', views.CampanhaView.as_view(), name='campanha_views'),
 ]

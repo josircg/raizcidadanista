19760dfabfeb616ecd1ff6539b58f0c71635d52f
Leonardo C. Santos <leonardocsantoss@gmail.com>
2016-07-11 08:24:11 -0300

    Prestação de Contas #93

--- raizcidadanista/settings/settings.py
+++ raizcidadanista/settings/settings.py
@@ -47,6 +47,11 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = False
 
+# Decimal
+USE_THOUSAND_SEPARATOR = True
+DECIMAL_SEPARATOR = ','
+THOUSAND_SEPARATOR = '.'
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')

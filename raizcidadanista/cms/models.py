b6c30561560e02f282944883249d8c7ef632036c
Leonardo C. Santos <leonardocsantoss@gmail.com>
2016-07-29 11:38:52 -0300

    ignore templates

--- raizcidadanista/cms/models.py
+++ raizcidadanista/cms/models.py
@@ -89,7 +89,7 @@ class Article(models.Model):
         return True
 
     def get_images(self):
-        rex = re.compile(r'(<img )(.*)(src=")([a-zA-Z0-9- _/\.()]*)(".*)(/?>)')
+        rex = re.compile(r'(<img )(.*)(src=")([a-zA-Z0-9- _/\.():\?\&\=]*)(".*)(>)')
         images = []
         for img_rex in rex.findall(self.header):
             images.append(img_rex[3])

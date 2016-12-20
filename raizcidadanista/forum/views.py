24c1746577bc8637d704e3b3995467a57f08390d
Leonardo C. Santos <leonardocsantoss@gmail.com>
2016-12-20 19:32:14 -0300

    Paginação do tópico #134

--- raizcidadanista/forum/views.py
+++ raizcidadanista/forum/views.py
@@ -834,7 +834,9 @@ class TopicoView(DetailView):
         context = super(TopicoView, self).get_context_data(**kwargs)
 
         conversas_list = self.object.conversa_set.filter(conversa_pai=None)
-        first_conversa = conversas_list[0]
+        first_conversa = None
+        if conversas_list.count() >= 1:
+            first_conversa = conversas_list[0]
         # A proposta tem que aparecer sempre no timeline do tópico: se já tiver concluída, mostrar pela ordenação da data.
         conversas_list = conversas_list.filter(Q(proposta=None) | Q(proposta__status='F') | Q(proposta__dt_encerramento__lt=datetime.now())).distinct()
         # Se estiver em aberto, mostrar no topo do tópico.
@@ -851,7 +853,7 @@ class TopicoView(DetailView):
             conversas = paginator.page(paginator.num_pages)
 
         # Adicionar a primeira conversa nas demais páginas da paginação
-        if conversas.index != 1:
+        if conversas.index != 1 and first_conversa:
             conversas.object_list = list(conversas.object_list)
             conversas.object_list.insert(0, first_conversa)
 
